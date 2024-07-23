a586853cb500cfb40197fecee6fb418b0c78678f
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2024-07-23 14:08:59 +0200

    added command history system

--- src/components/terminal-commands/Input.jsx
+++ src/components/terminal-commands/Input.jsx
@@ -17,6 +17,7 @@ import { HandleNANO } from "../../methods/HandleNANO";
 
 function Input() {
   const [input, setInput] = useState("");
+  const [previousMessageIndex, setPreviousMessageIndex] = useState(0);
   const { state, dispatch } = useContext(MessageContext);
   const inputRef = useRef(null);
 
@@ -28,68 +29,78 @@ function Input() {
     init();
   }, []);
 
-  useEffect(() => {
-    const handleKeyDown = async (e) => {
-      if (e.key === "Enter") {
-        e.preventDefault();
-        dispatch({
-          type: "ADD_COMMAND",
-          payload: { text: input },
-        });
+  const handleKeyDown = async (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      dispatch({
+        type: "ADD_COMMAND",
+        payload: { text: input },
+      });
 
-        await HandleSetFiles(dispatch);
+      await HandleSetFiles(dispatch);
 
-        // save command to history based on input
-        const command = inputRef.current.value.split(" ")[0];
+      // save command to history based on input
+      const command = inputRef.current.value.split(" ")[0];
 
-        const commandHandlers = {
-          clear: () => HandleCLEAR(inputRef.current.value, dispatch),
-          help: () => HandleHELP(inputRef.current.value, state, dispatch),
-          ls: () => HandleLS(inputRef.current.value, state, dispatch),
-          cd: () => HandleCD(inputRef.current.value, state, dispatch),
-          mkdir: () => HandleMKDIR(inputRef.current.value, state, dispatch),
-          rmdir: () => HandleRMDIR(inputRef.current.value, state, dispatch),
-          touch: () => HandleTOUCH(inputRef.current.value, state, dispatch),
-          rm: () => HandleRM(inputRef.current.value, state, dispatch),
-          cat: () => HandleCAT(inputRef.current.value, state, dispatch),
-          nano: () => HandleNANO(inputRef.current.value, state, dispatch),
-          mv: () => HandleMV(inputRef.current.value, state, dispatch),
-          neofetch: () => Neofetch(inputRef.current.value, state, dispatch),
-          projects: () => Projects(inputRef.current.value, state, dispatch),
-        };
+      const commandHandlers = {
+        clear: () => HandleCLEAR(inputRef.current.value, dispatch),
+        help: () => HandleHELP(inputRef.current.value, state, dispatch),
+        ls: () => HandleLS(inputRef.current.value, state, dispatch),
+        cd: () => HandleCD(inputRef.current.value, state, dispatch),
+        mkdir: () => HandleMKDIR(inputRef.current.value, state, dispatch),
+        rmdir: () => HandleRMDIR(inputRef.current.value, state, dispatch),
+        touch: () => HandleTOUCH(inputRef.current.value, state, dispatch),
+        rm: () => HandleRM(inputRef.current.value, state, dispatch),
+        cat: () => HandleCAT(inputRef.current.value, state, dispatch),
+        nano: () => HandleNANO(inputRef.current.value, state, dispatch),
+        mv: () => HandleMV(inputRef.current.value, state, dispatch),
+        neofetch: () => Neofetch(inputRef.current.value, state, dispatch),
+        projects: () => Projects(inputRef.current.value, state, dispatch),
+      };
 
-        if (commandHandlers[command]) {
-          commandHandlers[command]();
-        } else {
-          HandleUndefined(inputRef.current.value, state, dispatch);
-        }
-
-        // clear input
-        setInput("");
-
-        // TODO: upgrade command history system
-      } else if (e.key === "ArrowUp") {
-        e.preventDefault();
-
-        // get previous command
-        if (state.commands.length === 0) return;
-        const previousCommand = state.commands[state.commands.length - 1];
-        setInput(previousCommand.text);
-      } else if (e.key === "ArrowDown") {
-        e.preventDefault();
-
-        // get next command
-        setInput("");
+      if (commandHandlers[command]) {
+        commandHandlers[command]();
+      } else {
+        HandleUndefined(inputRef.current.value, state, dispatch);
       }
-    };
+
+      // clear input
+      setInput("");
+      setPreviousMessageIndex(0);
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+
+      // get previous command
+      if (state.commands.length === 0) return;
+      if (previousMessageIndex + 1 <= state.commands.length) {
+        const previousCommand =
+          state.commands[state.commands.length - previousMessageIndex - 1];
+        setInput(previousCommand.text);
+        setPreviousMessageIndex(previousMessageIndex + 1);
+      }
+    } else if (e.key === "ArrowDown") {
+      e.preventDefault();
+
+      // get next command
+      if (state.commands.length === 0) return;
+      if (previousMessageIndex - 1 >= 1) {
+        const previousCommand =
+          state.commands[state.commands.length - previousMessageIndex + 1];
+        setInput(previousCommand.text);
+        setPreviousMessageIndex(previousMessageIndex - 1);
+      } else {
+        setInput("");
+        setPreviousMessageIndex(0);
+      }
+    }
+  };
+
+  useEffect(() => {
     // reset height of input
     inputRef.current.style.height = "auto";
 
     // set height of input to match height of text
     inputRef.current.style.height = inputRef.current.scrollHeight + "px";
-
-    // add event listener for keys
-    inputRef.current?.addEventListener("keydown", handleKeyDown);
 
     // scroll to bottom of input
     inputRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -100,8 +111,9 @@ function Input() {
     };
     // add click event listener to document so that it always focuses on input
     document.addEventListener("click", handleClick);
+
     return () => {
-      inputRef.current?.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("click", handleClick);
     };
   }, [input]);
 
@@ -120,6 +132,7 @@ function Input() {
         ref={inputRef}
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
     </div>
   );
